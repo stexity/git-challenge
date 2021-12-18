a59c20f269e4b2ad03cd0a0aea2a05c0f27dc19e
Muhammad Hammad <hammadarshad834@gmail.com>
2021-12-18 18:42:28 +0500

    refact: solution

--- task1.py
+++ task1.py
@@ -3,9 +3,9 @@ sentence1 = "Hi all, my name is Tom...I am originally from Australia."
 sentence2 = "I need to work very hard to learn more about algorithms in Python!"
 
 def solution(sentence):
-    for i in "!?',;.":
-        sentence = sentence.replace(i, ' ')
-    words = sentence.split()
+    for punc in "! ? ' , ; . ":
+        sentence = sentence.replace(punc, ' ')
+    words = sentence.split(' ')
     return round(sum(len(word) for word in words)/len(words),2)
 
 print(solution(sentence1))
